156c39d31406426eb8122737f1c06fba07609719
viktor <irvin1442@gmail.com>
2021-12-15 16:40:51 +0200

    Add callback for model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
 class Item < ApplicationRecord
+  validates :price, numericality: { greater_than: 0, allow_nil: true }
+  validates :name, :description, presence: true
+
+  after_initialize { p 'initialize' }
+  after_save { p 'save' }
+  after_create { p 'created' }
+  after_update { p 'updated' }
+  after_destroy { p 'Item was destroyed.' }
 end
